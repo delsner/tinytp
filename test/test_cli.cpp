c48063de023cd06310bf60d3f4d3a1138712c39d
Daniel Elsner <daniel.elsner@tum.de>
2023-04-12 12:50:19 +0100

    Another fix for win32 build

--- test/test_cli.cpp
+++ test/test_cli.cpp
@@ -64,24 +64,26 @@ protected:
 TEST_F(CliTestSuite, ParseEndToEnd) {
     std::unique_ptr<TinyTPRunner> runner;
     // write into db
-    std::vector<std::string> argv1{"tinytp",
-                                   "collect",
-                                   "--db",
-                                   dbFile,
-                                   "--jenkins",
-                                   testReport};
+    std::vector<std::string> argv1;
+    argv1.emplace_back("tinytp");
+    argv1.emplace_back("collect");
+    argv1.emplace_back("--db");
+    argv1.push_back(dbFile.string());
+    argv1.emplace_back("--jenkins");
+    argv1.push_back(testReport.string());
     std::vector<std::string_view> args1{argv1.begin() + 1, argv1.end()};
     runner = parse(args1);
     ASSERT_NE(dynamic_cast<TinyTPCollector *>(runner.get()), nullptr);
     ASSERT_EQ(runner->run(), 0);
 
     // read from db
-    std::vector<std::string> argv2{"tinytp",
-                                   "prio",
-                                   "--db",
-                                   dbFile,
-                                   "--output",
-                                   outputDir};
+    std::vector<std::string> argv2;
+    argv2.emplace_back("tinytp");
+    argv2.emplace_back("prio");
+    argv2.emplace_back("--db");
+    argv2.push_back(dbFile.string());
+    argv2.emplace_back("--output");
+    argv2.push_back(outputDir.string());
     std::vector<std::string_view> args2{argv2.begin() + 1, argv2.end()};
     runner = parse(args2);
     ASSERT_NE(dynamic_cast<TinyTPPrio *>(runner.get()), nullptr);
